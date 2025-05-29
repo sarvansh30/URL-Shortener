ae6f2a97d2c26d40e548bfa3b81cf5401ac276c9
sarvansh30 <sarvansh.pachori45@gmail.com>
2025-05-29 23:47:17 +0530

    working backend prototype done

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
+from typing import Optional
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 load_dotenv()
@@ -16,23 +18,61 @@ def getHome():
     return {"msg":"HEllow"}
 
 def generateID():
-    for i in range(1000000,10000000):
+    i=0
+    while True:
         yield i
-
+        i+=1
+gen = generateID()
 def base62Converter(slug):
     base62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    num=len(base62)
+    createdSlug=[]
+    if slug==0:
+        return base62[0]
+    while slug>0:
+        rem=slug%num
+        slug=slug//num
+        createdSlug.append(base62[rem])
     
+    return "".join(createdSlug)
 
-
-async def generateSlug(linkData,customInput=""):
-    if len(customInput)==0:
-        slug = next(generateID())
+async def generateSlug(linkData,customInput):
+    if customInput==None:
+        slug = next(gen)
         genSlug=base62Converter(slug)
-        return True
+        # print(genSlug)
+        await shortLinksDB.insert_one({"slug":genSlug,"link":linkData })
+        return genSlug
     else:
         res = await shortLinksDB.find_one({"slug":customInput})
         if res==None:
             shortLinksDB.insert_one({"slug":customInput,"link":linkData})
-            return True
+            return customInput
         else:
-            return False
+            return -1
+
+@app.post("/createShortLink")
+async def creatingShortLink(link:str,customInput:Optional[str]=None ):
+    
+    flag=link.find("https://")
+    if flag==-1:
+        link = "https://"+link
+
+    resp = await shortLinksDB.find_one({"link":link})
+    if resp:
+        return {"msg":"Shortlink already exists",
+                "stLink":f"smol/r/{resp["slug"]}"}
+    result = await generateSlug(link,customInput)
+    if result == -1:
+        return {"Msg":"This is alsready in use try another"}
+    return {"stLink":f"smol/r/{result}"}
+
+
+@app.get("/r/{slug}")
+async def redirectUser(slug:str):
+    resp = await shortLinksDB.find_one({"slug":slug})
+
+    if resp:
+        return RedirectResponse(url=resp["link"])
+    else:
+        raise HTTPException(status_code=404,detail="Shortlink not found")
