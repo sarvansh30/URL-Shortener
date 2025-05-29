09f32e61068a9b404759dad5813a4cbb3abf4a45
sarvansh30 <sarvansh.pachori45@gmail.com>
2025-05-30 02:47:00 +0530

    added excetion handling and http errors

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,8 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import os
 from typing import Optional
 from fastapi.responses import RedirectResponse
+from pymongo.errors import PyMongoError
+import re
 
 app = FastAPI()
 load_dotenv()
@@ -13,16 +15,13 @@ client = AsyncIOMotorClient(Backend_URL)
 db = client.get_database("URL-shortener")
 shortLinksDB = db["shortLinks"]
 
-@app.get("/")
-def getHome():
-    return {"msg":"HEllow"}
-
 def generateID():
     i=0
     while True:
         yield i
         i+=1
 gen = generateID()
+
 def base62Converter(slug):
     base62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     num=len(base62)
@@ -41,36 +40,53 @@ async def generateSlug(linkData,customInput):
         slug = next(gen)
         genSlug=base62Converter(slug)
         # print(genSlug)
-        await shortLinksDB.insert_one({"slug":genSlug,"link":linkData })
+        try:
+            await shortLinksDB.insert_one({"slug":genSlug,"link":linkData })
+        except PyMongoError as e:
+            raise HTTPException(status_code=500,detail="Error occurred inserting the shortLink, Try Again!!")
+        
         return genSlug
     else:
-        res = await shortLinksDB.find_one({"slug":customInput})
-        if res==None:
-            shortLinksDB.insert_one({"slug":customInput,"link":linkData})
-            return customInput
-        else:
-            return -1
+        try:
+            res = await shortLinksDB.find_one({"slug":customInput})
+            if res==None:
+                shortLinksDB.insert_one({"slug":customInput,"link":linkData})
+                return customInput
+            else:
+                raise HTTPException(status_code=409,detail="This custom shortlink is already in use. Try another.")
+        except PyMongoError as e:
+            raise HTTPException(status_code=500,detail="Error occurred finding or inserting the shortLink, Try Again!!")
+
 
 @app.post("/createShortLink")
 async def creatingShortLink(link:str,customInput:Optional[str]=None ):
     
+    if customInput and not re.match(r'^[A-Za-z0-9]{1,30}$',customInput):
+        raise HTTPException(status_code=400,detail="Use valid symbols for custom link with length ranging from 1 to 30 characters")
     flag=link.find("https://")
     if flag==-1:
         link = "https://"+link
 
-    resp = await shortLinksDB.find_one({"link":link})
+    try:
+        resp = await shortLinksDB.find_one({"link":link})
+    except PyMongoError as e:
+        raise HTTPException(status_code=409,detail="Server Error occurred trying to create short-link")
     if resp:
         return {"msg":"Shortlink already exists",
                 "stLink":f"smol/r/{resp["slug"]}"}
     result = await generateSlug(link,customInput)
     if result == -1:
-        return {"Msg":"This is alsready in use try another"}
+        raise HTTPException(status_code=400,detail="This link already in use try another custom link")
     return {"stLink":f"smol/r/{result}"}
+
 
 
 @app.get("/r/{slug}")
 async def redirectUser(slug:str):
-    resp = await shortLinksDB.find_one({"slug":slug})
+    try:
+        resp = await shortLinksDB.find_one({"slug":slug})
+    except PyMongoError as e:
+        raise HTTPException(status_code=500,detail="Error occurred in redirecting. Try Again!")
 
     if resp:
         return RedirectResponse(url=resp["link"])
